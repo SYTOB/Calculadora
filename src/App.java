290b23250f3cc2f605f05fb75c1cc70ca95a04fc
Diovani <diovanips@hotmail.com>
2022-05-15 00:04:11 -0300

    Format

--- src/App.java
+++ src/App.java
@@ -3,45 +3,42 @@ import java.lang.reflect.InvocationTargetException;
 
 import java.util.Scanner;
 
-public class App{
-    public static void main(String[] args) throws InstantiationException, IllegalAccessException, 
-    ClassNotFoundException, NoSuchMethodException, InvocationTargetException {
+public class App {
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException,
+            ClassNotFoundException, NoSuchMethodException, InvocationTargetException {
 
         String operacao;
-        double num1,num2;
-        
+        double num1, num2;
+
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Calculadora!");
         System.out.println("Informe o primeiro número: ");
-        num1 = scan.nextDouble(); 
+        num1 = scan.nextDouble();
 
         System.out.println("Escolha(digite) uma das operações [Somar - Subtrair - Multiplicar - Dividir]");
-        operacao = scan.next(); 
+        operacao = scan.next();
 
         System.out.println("Informe o segundo número: ");
-        num2 = scan.nextDouble();    
+        num2 = scan.nextDouble();
         scan.close();
 
-        executar(operacao,num1,num2);
+        executar(operacao, num1, num2);
 
-       
+    }
 
-    } 
-
-    private static void executar(String operacao, Double num1, Double num2) throws InstantiationException, IllegalAccessException, 
-    ClassNotFoundException, NoSuchMethodException, InvocationTargetException {
+    private static void executar(String operacao, Double num1, Double num2)
+            throws InstantiationException, IllegalAccessException,
+            ClassNotFoundException, NoSuchMethodException, InvocationTargetException {
 
         Object classInstance = Class.forName(operacao).getDeclaredConstructor().newInstance();
 
         Method execute = Class.forName(operacao).getDeclaredMethod(
-            "resultado",
-            new Class[] { Double.class, Double.class }
-        ); 
+                "resultado",
+                new Class[] { Double.class, Double.class });
 
         execute.invoke(classInstance, num1, num2);
-        
+
     }
 
 }
-
